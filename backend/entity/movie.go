81c9d03f35ac6611df520fae24efbc349a47f8d0
Kamonchanok-tnd <Kamonchanok.tnd@gmail.com>
2024-10-16 13:24:24 +0700

    first commit

--- backend/entity/movie.go
+++ backend/entity/movie.go
@@ -12,9 +12,8 @@ type Movie struct {
 	Director      string
 	Actor         string
 	Synopsis      string
-	ReleaseDate   string  // เปลี่ยนจาก string เป็น time.Time
-	Poster        []byte        `gorm:"type:blob"`                  // เก็บภาพโปสเตอร์เป็น blob
+	ReleaseDate   string  
+	Poster        []byte  `gorm:"type:blob"`     // เก็บภาพโปสเตอร์เป็น blob เนื่องจากมีความคมชัดมากกว่า
 
-	// ความสัมพันธ์กับ ShowTimes
 	Showtimes []ShowTimes `gorm:"foreignKey:MovieID"`
 }
